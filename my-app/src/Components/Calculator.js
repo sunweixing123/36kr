91ccc9d2a18fd8e6bf2df2c266a86fb71eef0711
sunweixing123 <359561000@qq.com>
2018-07-20 15:12:10 +0800

    react test

--- my-app/src/Components/Calculator.js
+++ my-app/src/Components/Calculator.js
@@ -1,16 +1,32 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
 import Boiling from '../Components/Boiling';
 // import Temperature from '../Components/TemperatureInput';
 import TemperatureInput from '../Components/TemperatureInput';
+function toCelsius(fahrenheit) {
+    return (fahrenheit - 32) * 5 / 9;
+}
+function toFahrenheit(celsius) {
+    return (celsius * 9 / 5) + 32;
+}
+function tryConvert(temperature, convert) {
+    const input = parseFloat(temperature);
+    if (Number.isNaN(input)) {
+        return '';
+    }
+    const output = convert(input);
+    const rounded = Math.round(output * 1000) / 1000;
+    return rounded.toString();
+}
 class Calculator extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         // this.handleChange = this.handleChange.bind(this);
         this.handleCelsiusChange = this.handleCelsiusChange.bind(this);
+        this.handleFahrenheitChange = this.handleFahrenheitChange.bind(this);
         // this.handleFahrenheitChange = this.handleFahrenheitChange.bind(this);
         this.state = {
-            temperature : '',
-            // scale: 'c'
+            temperature: '',
+            scale: 'c'
         }
     }
     // handleChange(e){
@@ -18,20 +34,22 @@ class Calculator extends Component {
     //         temperature : 'e.target.value'
     //     })
     // }
-    handleCelsiusChange(temperature){
+    handleCelsiusChange(temperature) {
         this.setState({
-            temperature
+            sccale: 'c', temperature
         })
     }
-    // handleFahrenheitChange(temperature){
-    //     this.setState({
-    //         temperature
-    //     })
-    // }
-    render(){
-        // const scale = this.state.scale;     
+    handleFahrenheitChange(temperature) {
+        this.setState({
+            scale: 'f', temperature
+        })
+    }
+    render() {
+        const scale = this.state.scale;
         const temperature = this.state.temperature;
-        // const celsius = this.state.celsius;
+        const celsius = scale === 'f' ? tryConvert(temperature, toCelsius) : temperature;
+        const fahrenheit = scale === 'c' ? tryConvert(temperature, toFahrenheit) : temperature;
+
 
         return (
             // <div>
@@ -44,19 +62,19 @@ class Calculator extends Component {
             //     <Boiling celsius={parseFloat(temperature)} />
             // </fieldset>
             <div>
-        <TemperatureInput
-        //   scale="c"
-          temperature={temperature}
-          onTemperatureChange={this.handleCelsiusChange} />
+                <TemperatureInput
+                    scale="c"
+                    temperature={celsius}
+                    onTemperatureChange={this.handleCelsiusChange} />
 
-        <TemperatureInput
-        //   scale="f"
-          temperature={temperature}
-          onTemperatureChange={this.handleCelsiusChange} />
+                <TemperatureInput
+                    scale="f"
+                    temperature={fahrenheit}
+                    onTemperatureChange={this.handleFahrenheitChange} />
 
-        <Boiling />
+                <Boiling celsius={parseFloat(celsius)} />
 
-      </div>
+            </div>
         )
     }
 }
